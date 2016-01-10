fdab1d995b2860ba3b815458d2fd5bf4dd7b4e74
Juan Antonio Guerra Montenegro <evanblackhill@gmail.com>
2016-01-10 23:43:55 +0000

    Object Update (0.0.4)
    - Code added on routes and views for the "Object" view. This view will
    show an object's features, images and description.
    - Partial "Object" view Bootstrapping.

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var tasks = require('./routes/items');
+var items = require('./routes/items');
+var obj = require('./routes/objects'); // AÑADIDA
 
 var app = express();
 
@@ -35,8 +36,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', routes);
-app.use('/items', tasks);
+app.use('/items', items);
 app.use('/users', users);
+app.use('/objects', obj); // AÑADIDA
 
 
 // catch 404 and forward to error handler
